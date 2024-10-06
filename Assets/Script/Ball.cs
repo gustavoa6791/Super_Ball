0b786cc74f0d6ca8153fc7034f9570ce75da842b
gustavoa6791 <gustavo.arango6791@hotmail.com>
2024-10-06 04:32:05 -0500

    camera movements

--- Assets/Script/Ball.cs
+++ Assets/Script/Ball.cs
@@ -1,12 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Ball : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
+        float[] initial = { -5f, 5f };
+        int Xramdon = (int)Random.Range(0, 1.9f);
+        int Yramdon = (int)Random.Range(0, 1.9f);
+        GetComponent<Rigidbody>().velocity = new Vector3(initial[Xramdon], 0, initial[Yramdon]);
         
     }
 
