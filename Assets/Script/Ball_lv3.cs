494a9ad941217d5b1b8536cfb5d3df7b60b242a8
sebaspapu <sebasnarutoxd@gmail.com>
2024-10-20 00:50:25 -0500

    feat: wall center and perf in oponent
    - se añade un muro como en el diseño original, para evitar que los players invadan al campo del otro. se mejora por otro lado la IA del player oponente para que siga el disco

---
+++ Assets/Script/Ball_lv3.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Ball_lv3 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        float[] initial = { -5f, 5f };
+        int Xramdon = (int)Random.Range(0, 1.9f);
+        int Yramdon = (int)Random.Range(0, 1.9f);
+        GetComponent<Rigidbody>().velocity = new Vector3(initial[Xramdon], 0, initial[Yramdon]);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
